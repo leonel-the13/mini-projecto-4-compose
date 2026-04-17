0485d769c1435c57eef5e2f1c96bad998413c7a6
vleonel <vleonel@vleonel.myguest.virtualbox.org>
2026-04-17 13:17:04 +0100

    my first commit

---
+++ src/main/java/com/example/mini_projecto_4_compose/MiniProjecto4ComposeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_projecto_4_compose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjecto4ComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjecto4ComposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_projecto_4_compose/MiniProjecto4ComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_projecto_4_compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjecto4ComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
